4233051880727df060e4ddbb2e5c66fdaca858fc
Aki Evans <50716261+AkiBae4891@users.noreply.github.com>
2019-08-09 11:55:27 -0400

    Add files via upload

---
+++ Acme.py
@@ -0,0 +1,15 @@
+
+class Samurai:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+samurai.__init__()
+
+
+
+
+name = 'sword'
+price = 600
+weight = 20
+flammability = 0.5
+identifier = random.randint(1000000, 9999999)
