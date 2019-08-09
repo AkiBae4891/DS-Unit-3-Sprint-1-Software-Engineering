f0f6ab5903d1ab069fc2272b3f2fcd8119c46565
Aki Evans <50716261+AkiBae4891@users.noreply.github.com>
2019-08-09 16:35:30 -0400

    Updated
    Correct format

--- Acme.py
+++ Acme.py
@@ -1,15 +1,9 @@
 
-class Samurai:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-samurai.__init__()
+class Product:
+    def __init__(self, name, price=10, weight=20, flammability=0.5, identifier=random.randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
 
-
-
-
-name = 'sword'
-price = 600
-weight = 20
-flammability = 0.5
-identifier = random.randint(1000000, 9999999)
