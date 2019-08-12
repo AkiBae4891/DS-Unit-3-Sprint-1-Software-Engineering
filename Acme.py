378b35dd3ef6707b8c2cfab75c9d247d10eb589c
Aki Evans <50716261+AkiBae4891@users.noreply.github.com>
2019-08-12 09:27:53 -0400

    Updated code correctly
    now works with open parenthesis

--- Acme.py
+++ Acme.py
@@ -1,6 +1,5 @@
 import random
 
-
 class Product:
     def __init__(self, name, price=10, weight=20, flammability=0.5, identifier=random.randint(1000000, 9999999)):
         self.name = name
@@ -9,7 +8,7 @@ class Product:
         self.flammability = flammability
         self.identifier = identifier
 
-    def stealability(self, price, weight):
+    def stealability(self):
         if self.price/self.weight < 0.5:
             return "Not so stealable..."
         elif self.price/self.weight >= 0.5 and self.price/self.weight < 1.0:
@@ -17,11 +16,33 @@ class Product:
         else:
             return "Very stealable!"
 
-    def explode(self, flammability, weight):
-        if flammability * weight < 10:
+    def explode(self):
+        if self.flammability * self.weight < 10:
             return "...fizzle."
-        elif flammability * weight >= 10 and flammability * weight < 50:
+        elif self.flammability * self.weight >= 10 and self.flammability * self.weight < 50:
             return "...boom!"
         else:
             return "...BABOOM!!"
 
+    def __repr__(self):
+        return self.name
+
+class BoxingGlove(Product):
+    def __init__(self, name, price=10, weight=10, flammability=0.5, identifier=random.randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+
+    def explode(self):
+        if self.flammability * self.weight:
+            return "...it's a glove."
+
+    def punch(self):
+        if self.weight < 5:
+            return "That tickles."
+        elif self.weight >= 5 and self.weight < 15:
+            return "Hey that hurt!"
+        else:
+            return "Ouch!"
